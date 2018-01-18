0d392a71884fb5f00800b63f676438fc587d5a46
Federico Toscano <federico@dolen.org>
2018-01-18 16:07:28 +0000

    location autocompleter for availability

--- app/assets/javascripts/location-autocomplete.js
+++ app/assets/javascripts/location-autocomplete.js
@@ -1,20 +1,25 @@
 $(function() {
-  if ($("#location").length) {
-    function fillCoordinatesFields() {
-      var location = autocomplete.getPlace();
+  $locationAutocompleteInput = $("#location");
 
-      $("#lat").val(location["geometry"]["location"].lat());
-      $("#lng").val(location["geometry"]["location"].lng());
-    }
+  if ($locationAutocompleteInput.length == 0) return
 
-    var autocomplete;
-    var input = document.getElementById("location");
-    var options = {
-      componentRestrictions: { country: "uk" },
-      types: ["(regions)"] // (cities)
-    };
-
-    autocomplete = new google.maps.places.Autocomplete(input, options);
-    autocomplete.addListener("place_changed", fillCoordinatesFields);
+  function fillCoordinatesFields() {
+    var location = autocomplete.getPlace();
+    $('[data-location-name]').val(location['name']);
+    $('[data-location-address]').val(location['formatted_address']);
+    $('[data-location-latitude]').val(location['geometry']['location'].lat());
+    $('[data-location-longitude]').val(location['geometry']['location'].lng());
+    $('[data-location-gmaps-place-id]').val(location['place_id']);
   }
+
+  var autocomplete;
+  var input = $locationAutocompleteInput.get()[0];
+  var locationType = input.dataset.locationType || '(regions)'
+  var options = {
+    componentRestrictions: { country: "uk" },
+    types: [locationType]
+  };
+
+  autocomplete = new google.maps.places.Autocomplete(input, options);
+  autocomplete.addListener("place_changed", fillCoordinatesFields);
 });
