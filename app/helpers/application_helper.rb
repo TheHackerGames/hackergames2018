fa34a2f8a92b5605635a9e4d772896cb30d79907
Federico Toscano <federico@dolen.org>
2018-01-18 13:47:54 +0000

    availabilities table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def format_time(time, format: :long)
+    return unless time.present?
+    if format.is_a?(String)
+      time.strftime(format)
+    else
+      time.to_formatted_s(format)
+    end
+  end
 end
