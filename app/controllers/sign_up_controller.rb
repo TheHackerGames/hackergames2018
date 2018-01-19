47834b10f07c2492f7f61afb28284751a88ff11b
Pedro Chambino <pchambino@gmail.com>
2018-01-19 14:43:20 +0000

    Rewire sign up flow

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -2,11 +2,9 @@ class SignUpController < ApplicationController
   before_action :log_current_state
   after_action :log_current_state
 
-  SIGN_UP_STEPS = %i[type_step meet_type_step name_step email_step sign_up]
-  before_action :skip_sign_up_steps, if: :signed_in?, only: SIGN_UP_STEPS
-  before_action :start_from_beginning, unless: :signed_in?, except: SIGN_UP_STEPS
-
   def type_step
+    return redirect_to action: :location_step if signed_in?
+
     new_state
   end
 
@@ -27,39 +25,6 @@ class SignUpController < ApplicationController
     merge_state(meet_user_type: params[:meet_user_type]) if params.include? :meet_user_type
   end
 
-  def email_step
-    merge_state(first_name: params[:first_name]) if params.include? :first_name
-    merge_state(last_name: params[:last_name]) if params.include? :last_name
-
-    unless current_state.include? :first_name
-      flash[:alert] = 'Missing name'
-      return redirect_to action: :name_step
-    end
-  end
-
-  def sign_up
-    unless params.include? :email
-      flash[:alert] = 'Missing email'
-      return redirect_to action: :email_step
-    end
-
-    user = User.create(
-      name: current_state[:first_name],
-      email: params[:email],
-      password: 'carwow',
-    )
-
-    unless user.persisted?
-      logger.warn "  User errors: #{user.errors.full_messages}"
-      flash[:alert] = "Failed to create account 😢 #{user.errors.full_messages.join('<br>')}"
-      return redirect_to action: :email_step
-    end
-
-    sign_in user
-
-    redirect_to action: :photo_step
-  end
-
   def photo_step
     @name = current_state[:first_name]
     @image = Image.new
@@ -67,11 +32,15 @@ class SignUpController < ApplicationController
 
   def upload_photo
     image = Image.create(file: params.dig(:image, :file))
-    if image.persisted? && current_user.update(image: image)
-      redirect_to action: :location_step
+    if image.persisted?
+      merge_state(image_id: image.id)
+      if current_state[:user_type] == 'civilian'
+        redirect_to action: :location_step
+      else
+        redirect_to action: :email_step
+      end
     else
       logger.warn "  Image errors: #{image.errors.full_messages}"
-      logger.warn "  User errors: #{current_user.errors.full_messages}"
       flash[:alert] = 'Invalid file'
       redirect_to action: :photo_step
     end
@@ -118,25 +87,66 @@ class SignUpController < ApplicationController
     end
   end
 
-  def create_availability
-    start_datetime = DateTime.parse "#{current_state[:date]} #{params[:start_time]}"
-    end_datetime = DateTime.parse "#{current_state[:date]} #{params[:end_time]}"
+  def email_step
+  end
 
-    @availability = Availability.create!(
+  def sign_up
+    unless params.include? :email
+      flash[:alert] = 'Missing email'
+      return redirect_to action: :email_step
+    end
+
+    user = User.create(
+      name: current_state[:first_name],
+      email: params[:email],
+      password: 'carwow',
+      image_id: current_state[:image_id],
+    )
+
+    unless user.persisted?
+      logger.warn "  User errors: #{user.errors.full_messages}"
+      flash[:alert] = "Failed to create account 😢 #{user.errors.full_messages.join('<br>')}"
+      return redirect_to action: :email_step
+    end
+
+    sign_in user
+
+    if current_state[:user_type] == 'civilian'
+      create_availability
+    else
+      redirect_to search_form_availabilities_path
+    end
+  end
+
+  def create_availability
+    if params.include? :start_datetime
+      merge_state(start_datetime: DateTime.parse("#{current_state[:date]} #{params[:start_time]}"))
+    end
+
+    if params.include? :end_datetime
+      merge_state(end_datetime: DateTime.parse("#{current_state[:date]} #{params[:end_time]}"))
+    end
+
+    return redirect_to action: :email_step unless signed_in?
+
+    availability = Availability.create(
       user: current_user,
       name: current_state[:shop_name],
       address: current_state[:shop_address],
       latitude: current_state[:latitude],
       longitude: current_state[:longitude],
       gmaps_place_id: current_state[:gmaps_place_id],
-      start_datetime: start_datetime,
-      end_datetime: end_datetime,
+      start_datetime: current_state[:start_datetime],
+      end_datetime: current_state[:end_datetime],
     )
 
-    redirect_to action: :final_step
-  end
+    unless availability.persisted?
+      logger.warn "  Availability errors: #{availability.errors.full_messages}"
+      flash[:alert] = "Failed to setup date and place 😢 #{availability.errors.full_messages.join('<br>')}"
+      return redirect_to action: :time_step
+    end
 
-  def final_step
+    redirect_to profile_path
   end
 
   private
