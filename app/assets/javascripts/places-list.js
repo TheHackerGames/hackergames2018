6e015913906a8d451601ed854a17cced59fba232
Vincent Navetat <vincent.navetat@carwow.co.uk>
2018-01-19 10:09:14 +0000

    List places for current location

--- app/assets/javascripts/places-list.js
+++ app/assets/javascripts/places-list.js
@@ -1,7 +1,4 @@
 $(function() {
-  $placesList = $("[data-places-list]");
-  if ($placesList.length == 0) return;
-
   function getPlaces() {
     var coordinates = {
       lat: $placesList.data("lat"),
@@ -24,26 +21,40 @@ $(function() {
 
   function callback(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
+      $results = results;
       for (var i = 0; i < results.length; i++) {
         console.log(results[i]);
-        // createMarker(results[i]);
+
+        $placesList.append(
+          "<li class='mb3'><a href='javascript:;' data-place-id='" +
+            i +
+            "'><div>" +
+            results[i].name +
+            "</div><div>" +
+            results[i].vicinity +
+            "</div></a></li>"
+        );
       }
     }
   }
 
+  $placesList = $("[data-places-list]");
+  if ($placesList.length == 0) return;
+
+  $results = [];
   getPlaces();
 
-  //
-  // function createMarker(place) {
-  //   var placeLoc = place.geometry.location;
-  //   var marker = new google.maps.Marker({
-  //     map: map,
-  //     position: place.geometry.location
-  //   });
-  //
-  //   google.maps.event.addListener(marker, "click", function() {
-  //     infowindow.setContent(place.name);
-  //     infowindow.open(map, this);
-  //   });
-  // }
+  $(document).on("click", "[data-place-id]", function() {
+    var placeId = $(this).data("place-id");
+
+    $("[data-location-name]").val($results[placeId]["name"]);
+    $("[data-location-address]").val($results[placeId]["vicinity"]);
+    $("[data-location-latitude]").val(
+      $results[placeId]["geometry"]["location"].lat()
+    );
+    $("[data-location-longitude]").val(
+      $results[placeId]["geometry"]["location"].lng()
+    );
+    $("[data-location-gmaps-place-id]").val($results[placeId]["place_id"]);
+  });
 });
